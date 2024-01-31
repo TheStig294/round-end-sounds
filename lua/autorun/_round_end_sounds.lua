5f715aec19143e6eceb1b423b20dbc3d2b5aa532
The Stig <thestig294@gmail.com>
2024-01-31 16:42:33 +0000

    Added 2 new convars and added several new win sounds for new roles

--- lua/autorun/_round_end_sounds.lua
+++ lua/autorun/_round_end_sounds.lua
@@ -1,5 +1,8 @@
+local soundNameMessageCvar = CreateConVar("ttt_roundendsounds_sound_name_message", 0, FCVAR_REPLICATED, "Whether a message in chat displays the name of the sound playing", 0, 1)
+
 if engine.ActiveGamemode() == "terrortown" and SERVER then
     CreateConVar("ttt_roundendsounds", "1", nil, "Turns on/off round end sounds playing at all. 1 = on, 0 = off", 0, 1)
+    local teamWinSoundCvar = CreateConVar("ttt_roundendsounds_team_win_sound", 0, nil, "Whether an innocent/traitor team win sound should play instead of a win sound for the winners and a lose sound for the losers", 0, 1)
     util.AddNetworkString("RoundEndSoundsWin")
     util.AddNetworkString("RoundEndSoundsPlay")
     local sounds = {}
@@ -47,7 +50,6 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
         wins["zombie"] = WIN_ZOMBIE
         wins["clown"] = WIN_CLOWN
         wins["monster"] = WIN_MONSTER
-        wins["lover"] = WIN_LOVER
         wins["vampire"] = WIN_VAMPIRE
         wins["bees"] = WIN_BEES
         wins["bee"] = WIN_BEE
@@ -55,6 +57,13 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
         wins["communist"] = WIN_COMMUNIST
         wins["frenchman"] = WIN_FRENCHMAN
         wins["cupid"] = WIN_CUPID
+        wins["sponge"] = WIN_SPONGE
+        wins["arsonist"] = WIN_ARSONIST
+        wins["hivemind"] = WIN_HIVEMIND
+        wins["vindicator"] = WIN_VINDICATOR
+        wins["detectoclown"] = WIN_DETECTOCLOWN
+        wins["krampus"] = WIN_KRAMPUS
+        wins["thething"] = WIN_THETHING
         -- Sound played depends on who wins
         local winningTeam = "noteam"
         local chosenSound = "nosound"
@@ -101,9 +110,19 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
         end
 
         if (winningTeam == "innocent" and not innocentSound) or (winningTeam == "traitor" and not traitorSound) or (winningTeam == "time" and not innocentSound) then
-            -- Choose a win/lose sound if the innocents or traitors win, or the time runs out (because it is its own win condition, but displays as an innocent win)
-            winSound = "ttt_round_end_sounds/win/" .. sounds["win"][math.random(#sounds["win"])]
-            lossSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
+            -- Play an innocent/traitor team win sound instead if the convar is enabled
+            -- (Sounds need to be added to an innocent_team or traitor_team sound folder to work)
+            if teamWinSoundCvar:GetBool() then
+                if winningTeam == "innocent" then
+                    chosenSound = "ttt_round_end_sounds/innocent_team/" .. sounds["innocent_team"][math.random(#sounds["innocent_team"])]
+                elseif winningTeam == "traitor" then
+                    chosenSound = "ttt_round_end_sounds/traitor_team/" .. sounds["traitor_team"][math.random(#sounds["traitor_team"])]
+                end
+            else
+                -- Choose a win/lose sound if the innocents or traitors win, or the time runs out (because it is its own win condition, but displays as an innocent win)
+                winSound = "ttt_round_end_sounds/win/" .. sounds["win"][math.random(#sounds["win"])]
+                lossSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
+            end
         elseif winningTeam ~= "noteam" and sounds[winningTeam] ~= nil then
             -- Choose a random sound from the winning team's pool of sounds
             chosenSound = "ttt_round_end_sounds/" .. winningTeam .. "/" .. sounds[winningTeam][math.random(#sounds[winningTeam])]
@@ -113,6 +132,12 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
         elseif winningTeam == "bee" then
             -- If it's a bee win, and there are no bee win sounds, choose a random bees win sound
             chosenSound = "ttt_round_end_sounds/bees/" .. sounds["bees"][math.random(#sounds["bees"])]
+        elseif winningTeam == "sponge" then
+            -- If it's a sponge win, and there are no sponge win sounds, choose a random jester win sound
+            chosenSound = "ttt_round_end_sounds/jester/" .. sounds["jester"][math.random(#sounds["jester"])]
+        elseif winningTeam == "detectoclown" then
+            -- If it's a detectoclown win, and there are no detectoclown win sounds, choose a random clown win sound
+            chosenSound = "ttt_round_end_sounds/clown/" .. sounds["clown"][math.random(#sounds["clown"])]
         elseif sounds["loss"][1] ~= nil and result ~= WIN_NONE then
             -- If a win condition happens that's not in the "wins" table, (E.g. a new role's win), choose a random 'loss' sound to play for everyone
             chosenSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
@@ -144,9 +169,18 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
 end
 
 if engine.ActiveGamemode() == "terrortown" and CLIENT then
+    local function PlayRoundEndSound(snd)
+        surface.PlaySound(snd)
+
+        if soundNameMessageCvar:GetBool() then
+            snd = string.sub(snd, 22)
+            chat.AddText("Playing sound:\n" .. snd)
+        end
+    end
+
     net.Receive("RoundEndSoundsPlay", function()
         local snd = net.ReadString()
-        surface.PlaySound(snd)
+        PlayRoundEndSound(snd)
     end)
 
     -- Play the win sound, if any, on the client so only that player hears it
@@ -168,32 +202,32 @@ if engine.ActiveGamemode() == "terrortown" and CLIENT then
 
         -- If a special role won, play one of that role's special win sounds
         if chosenSound ~= "nosound" then
-            surface.PlaySound(chosenSound)
+            PlayRoundEndSound(chosenSound)
             -- Old man win/loss
         elseif ply:GetNWBool("OldManWinSound", false) and oldmanWinSound ~= "nosound" then
-            surface.PlaySound(oldmanWinSound)
+            PlayRoundEndSound(oldmanWinSound)
         elseif ply:GetNWBool("OldManLossSound", false) and oldmanLossSound ~= "nosound" then
-            surface.PlaySound(oldmanLossSound)
+            PlayRoundEndSound(oldmanLossSound)
         elseif winSound ~= "nosound" and lossSound ~= "nosound" and CR_VERSION then
             -- When Custom roles is installed
             if (winningTeam == "innocent" or winningTeam == "time") and ply.IsInnocentTeam and ply:IsInnocentTeam() then
-                surface.PlaySound(winSound)
+                PlayRoundEndSound(winSound)
             elseif winningTeam == "traitor" and ply.IsTraitorTeam and ply:IsTraitorTeam() then
-                surface.PlaySound(winSound)
+                PlayRoundEndSound(winSound)
             elseif ply:GetNWBool("OldManWinSound", false) then
                 -- If there's no oldmanwin sounds, play an ordinary win sound
-                surface.PlaySound(winSound)
+                PlayRoundEndSound(winSound)
             else
-                surface.PlaySound(lossSound)
+                PlayRoundEndSound(lossSound)
             end
         elseif winSound ~= "nosound" and lossSound ~= "nosound" then
             -- When Custom roles isn't installed
             if (winningTeam == "innocent" or winningTeam == "time") and (ply:GetRole() == ROLE_INNOCENT or ply:GetRole() == ROLE_DETECTIVE) then
-                surface.PlaySound(winSound)
+                PlayRoundEndSound(winSound)
             elseif winningTeam == "traitor" and ply:GetRole() == ROLE_TRAITOR then
-                surface.PlaySound(winSound)
+                PlayRoundEndSound(winSound)
             else
-                surface.PlaySound(lossSound)
+                PlayRoundEndSound(lossSound)
             end
         end
     end)
