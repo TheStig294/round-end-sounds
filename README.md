84ef12512908f8582c04a46d60a59b6f6ecae7ae
thestig294 <thestig294@gmail.com>
2024-11-24 14:48:08 +1030

    Major code cleanup, updates are no longer needed for any newly added wins

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Round End Sounds (Win/Lose Sounds)
+
 **Add some sounds for when you win or lose a round in TTT**, fully compatible with Custom Roles!\
 (Also works just fine without it)\
 \
@@ -8,6 +9,7 @@ If a custom role wins, a random sound will play that only does when that role wi
 (E.g. a jester win will instead play a random jester win sound)
 
 ## Adding your own sounds (Optional)
+
 *If you're hosting a game through the main menu*\
 Navigate to: C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod, or wherever you have Garry's Mod installed.\
 Create a folder called "sound" and inside that, a folder called "ttt_round_end_sounds".\
@@ -16,7 +18,7 @@ Create a folder called "win" or "loss" or for a custom role sound, the name of t
 Place your sound(s) in the folder!\
 \
 If your sound isn't working, it might not be in a format Garry's Mod can read, try this:\
-Go to this website: https://onlineaudioconverter.com \
+Go to this website: <https://onlineaudioconverter.com> \
 Click and drag your sound in, then above "Sample Rate", click on "44.1 kHz" and click convert\
 Put your newly converted sound in the folder!\
 \
@@ -26,7 +28,7 @@ Do the above, but instead navigate to your server's "garrysmod" folder.\
 *If you're a mod-maker*\
 Any mod contaning:*sound/ttt_round_end_sounds/[win/loss/customrolename]/[soundfile(s)]* will automatically have its sounds played by this mod.\
 \
-Also, there is a**client-side** hook avaliable for adding sounds to user-made custom roles, or adding sounds to individual innocent/traitor roles.\
+Also, there is a **client-side** hook avaliable for adding sounds to user-made custom roles, or adding sounds to individual innocent/traitor roles.\
 The hook passes the player the sound is playing on (ply) and the win enumerator of the role that won (result).\
 Return the path to the sound that will play for that player, relative to the sounds folder.\
 \
@@ -45,6 +47,7 @@ Here are two examples:
 ```
 
 ## Turning sounds on or off
+
 **Sounds are NOT disabled until you restart the server or change maps**\
 \
 *If you're hosting a game through the main menu*\
@@ -65,6 +68,7 @@ Alternatively, any line from the list below can be added to your listenserver.cf
 \
 *If you're hosting a dedicated server*\
 Add any of the following on separate lines in your server's server.cfg:
+
 ```
 ttt_roundendsounds_bees_not_the_bees.mp3 0
 
@@ -99,9 +103,6 @@ ttt_roundendsounds_loss_sad_violin.mp3 0
 ttt_roundendsounds_loss_smallest_violin.mp3 0
 ttt_roundendsounds_loss_wasted.mp3 0
 
-ttt_roundendsounds_oldmanloss_yogpod_old_man.mp3 0
-ttt_roundendsounds_oldmanwin_tranzit.mp3 0
-
 ttt_roundendsounds_thething_echoing_laugh.mp3 0
 
 ttt_roundendsounds_vampire_phantom_of_the_opera.mp3 0
@@ -124,12 +125,14 @@ ttt_roundendsounds_win_yippee.mp3 0
 ttt_roundendsounds_zombie_zombies_round_end.mp3 0
 ttt_roundendsounds_zombie_zombies_round_start.mp3 0
 ```
+
 The format for your own sounds is the same:*ttt_roundendsounds_[win/loss/customrolename]_[filename] 0*\
 \
 *ttt_roundendsounds 0* will turn the mod off, and no sounds will play.\
 *ttt_roundendsounds 1* will turn it back on.
 
 ## Convars/Options
+
 *ttt_roundendsounds_sound_name_message 0*\
 Whether a message in chat displays the name of the sound playing\
 \
@@ -138,10 +141,12 @@ Whether an innocent/traitor team win sound should play instead of a win sound fo
 Place your sounds in an "innocent_team" and "traitor_team" folder, as opposed to the usual "win" and "loss" folders.
 
 ## Credits
-Musical notes in the workshop icon is from: https://game-icons.net/ \
+
+Musical notes in the workshop icon is from: <https://game-icons.net/> \
 \
-"Long Laugh 1" by ryanconway from: https://freesound.org/people/ryanconway/sounds/239576/ \
-Licenced under CC BY 4.0: https://creativecommons.org/licenses/by/4.0/
+"Long Laugh 1" by ryanconway from: <https://freesound.org/people/ryanconway/sounds/239576/> \
+Licenced under CC BY 4.0: <https://creativecommons.org/licenses/by/4.0/>
 
 ## Steam Workshop Link
-https://steamcommunity.com/sharedfiles/filedetails/?id=2585444967
+
+<https://steamcommunity.com/sharedfiles/filedetails/?id=2585444967>
--- lua/autorun/_round_end_sounds.lua
+++ lua/autorun/_round_end_sounds.lua
@@ -4,7 +4,6 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
     CreateConVar("ttt_roundendsounds", "1", nil, "Turns on/off round end sounds playing at all. 1 = on, 0 = off", 0, 1)
     local teamWinSoundCvar = CreateConVar("ttt_roundendsounds_team_win_sound", 0, nil, "Whether an innocent/traitor team win sound should play instead of a win sound for the winners and a lose sound for the losers", 0, 1)
     util.AddNetworkString("RoundEndSoundsWin")
-    util.AddNetworkString("RoundEndSoundsPlay")
     local sounds = {}
     local _, foundDirectories = file.Find("sound/ttt_round_end_sounds/*", "GAME")
 
@@ -21,13 +20,6 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
             -- Creating a convar for each sound to turn it off
             local convar = CreateConVar("ttt_roundendsounds_" .. dir .. "_" .. fileName, "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Set this to 0 to turn off this round end sound", 0, 1)
 
-            -- Adding a console command to play the sound
-            concommand.Add("ttt_roundendsounds_" .. dir .. "_" .. fileName .. "_playsound", function(plyexe, cc, arg)
-                net.Start("RoundEndSoundsPlay")
-                net.WriteString("ttt_round_end_sounds/" .. dir .. "/" .. fileName)
-                net.Send(plyexe)
-            end)
-
             -- Removing any round end sounds that have been turned off
             if not convar:GetBool() then
                 table.insert(disabledSounds, fileName)
@@ -39,106 +31,48 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
         end
     end
 
-    hook.Add("TTTEndRound", "TTTRoundEndSounds", function(result)
-        -- Hard-coded list of win condition names and their enumerators cause idk how to dynamically get them
-        local wins = {}
-        wins["time"] = WIN_TIMELIMIT
-        wins["innocent"] = WIN_INNOCENT
-        wins["traitor"] = WIN_TRAITOR
-        wins["jester"] = WIN_JESTER
-        wins["killer"] = WIN_KILLER
-        wins["zombie"] = WIN_ZOMBIE
-        wins["clown"] = WIN_CLOWN
-        wins["monster"] = WIN_MONSTER
-        wins["vampire"] = WIN_VAMPIRE
-        wins["bees"] = WIN_BEES
-        wins["bee"] = WIN_BEE
-        wins["boxer"] = WIN_BOXER
-        wins["communist"] = WIN_COMMUNIST
-        wins["frenchman"] = WIN_FRENCHMAN
-        wins["cupid"] = WIN_CUPID
-        wins["sponge"] = WIN_SPONGE
-        wins["arsonist"] = WIN_ARSONIST
-        wins["hivemind"] = WIN_HIVEMIND
-        wins["vindicator"] = WIN_VINDICATOR
-        wins["detectoclown"] = WIN_DETECTOCLOWN
-        wins["krampus"] = WIN_KRAMPUS
-        wins["thething"] = WIN_THETHING
+    hook.Add("TTTEndRound", "TTTRoundEndSounds", function(winEnum)
+        local winEnumToTeamName = {}
+
+        -- List of win condition names and their enumerators
+        -- Have to do this at the end of each round because mods could add a new win condition mid-round, e.g. Mal's bees win randomat
+        for key, value in pairs(_G) do
+            if isnumber(value) and key:StartsWith("WIN_") then
+                key = key:sub(5):lower()
+                winEnumToTeamName[value] = key
+            end
+        end
+
+        -- Other appropriate win sound folders to pick a sound from if the team's win sound folder is empty
+        local backupTeamWinSound = {
+            innocent = "innocent_team",
+            timelimit = "innocent_team",
+            traitor = "traitor_team",
+            monster = "zombie",
+            bee = "bees",
+            sponge = "jester",
+            detectoclown = "clown"
+        }
+
         -- Sound played depends on who wins
-        local winningTeam = "noteam"
+        local winningTeam = winEnumToTeamName[winEnum] or "noteam"
+        local backupTeam = backupTeamWinSound[winningTeam]
         local chosenSound = "nosound"
         local winSound = "nosound"
         local lossSound = "nosound"
-        local oldmanWinSound = "nosound"
-        local oldmanLossSound = "nosound"
-        local oldman = false
 
-        -- Getting the name of the team that won
-        for i, winNumber in pairs(wins) do
-            if result == winNumber then
-                winningTeam = table.KeyFromValue(wins, winNumber)
-            end
-        end
-
-        -- Handling an old man win/loss, which can have its own set of win/loss sounds
-        for i, ply in ipairs(player.GetAll()) do
-            if ply:GetRole() == ROLE_OLDMAN and ply:Alive() and not ply:IsSpec() then
-                ply:SetNWBool("OldManWinSound", true)
-                oldman = true
-            elseif ply:GetRole() == ROLE_OLDMAN and ply:IsSpec() and not ply:Alive() then
-                ply:SetNWBool("OldManLossSound", true)
-                oldman = true
-            end
-        end
-
-        if sounds["oldmanwin"][1] ~= nil and oldman then
-            oldmanWinSound = "ttt_round_end_sounds/oldmanwin/" .. sounds["oldmanwin"][math.random(#sounds["oldmanwin"])]
-        elseif sounds["oldmanloss"][1] ~= nil and oldman then
-            oldmanLossSound = "ttt_round_end_sounds/oldmanloss/" .. sounds["oldmanloss"][math.random(#sounds["oldmanloss"])]
-        end
-
-        -- Checking if there's enabled sounds in innocent/traitor sound folders, which override the win/loss sound logic and have a innocent/traitor win sound play for everyone instead
-        local innocentSound = false
-        local traitorSound = false
-
-        if istable(sounds["innocent"]) and not table.IsEmpty(sounds["innocent"]) then
-            innocentSound = true
-        end
-
-        if istable(sounds["traitor"]) and not table.IsEmpty(sounds["traitor"]) then
-            traitorSound = true
-        end
-
-        if (winningTeam == "innocent" and not innocentSound) or (winningTeam == "traitor" and not traitorSound) or (winningTeam == "time" and not innocentSound) then
-            -- Play an innocent/traitor team win sound instead if the convar is enabled
-            -- (Sounds need to be added to an innocent_team or traitor_team sound folder to work)
-            if teamWinSoundCvar:GetBool() then
-                if winningTeam == "innocent" then
-                    chosenSound = "ttt_round_end_sounds/innocent_team/" .. sounds["innocent_team"][math.random(#sounds["innocent_team"])]
-                elseif winningTeam == "traitor" then
-                    chosenSound = "ttt_round_end_sounds/traitor_team/" .. sounds["traitor_team"][math.random(#sounds["traitor_team"])]
-                end
-            else
-                -- Choose a win/lose sound if the innocents or traitors win, or the time runs out (because it is its own win condition, but displays as an innocent win)
-                winSound = "ttt_round_end_sounds/win/" .. sounds["win"][math.random(#sounds["win"])]
-                lossSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
-            end
+        if (winningTeam == "innocent" or winningTeam == "timelimit" or winningTeam == "traitor") and not teamWinSoundCvar:GetBool() then
+            -- Choose a win/lose sound if the innocents or traitors win, or the time runs out (Time running out is also an innocent win, but its own winEnum)
+            winSound = "ttt_round_end_sounds/win/" .. sounds["win"][math.random(#sounds["win"])]
+            lossSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
         elseif winningTeam ~= "noteam" and sounds[winningTeam] ~= nil then
             -- Choose a random sound from the winning team's pool of sounds
             chosenSound = "ttt_round_end_sounds/" .. winningTeam .. "/" .. sounds[winningTeam][math.random(#sounds[winningTeam])]
-        elseif winningTeam == "monster" then
-            -- If the monster team wins, and there are no monster team sounds, choose a random zombies sound
-            chosenSound = "ttt_round_end_sounds/zombie/" .. sounds["zombie"][math.random(#sounds["zombie"])]
-        elseif winningTeam == "bee" then
-            -- If it's a bee win, and there are no bee win sounds, choose a random bees win sound
-            chosenSound = "ttt_round_end_sounds/bees/" .. sounds["bees"][math.random(#sounds["bees"])]
-        elseif winningTeam == "sponge" then
-            -- If it's a sponge win, and there are no sponge win sounds, choose a random jester win sound
-            chosenSound = "ttt_round_end_sounds/jester/" .. sounds["jester"][math.random(#sounds["jester"])]
-        elseif winningTeam == "detectoclown" then
-            -- If it's a detectoclown win, and there are no detectoclown win sounds, choose a random clown win sound
-            chosenSound = "ttt_round_end_sounds/clown/" .. sounds["clown"][math.random(#sounds["clown"])]
-        elseif sounds["loss"][1] ~= nil and result ~= WIN_NONE then
+        elseif backupTeam and sounds[backupTeam] ~= nil then
+            -- If there's an alternative folder to get this winEnum's win sound from, and it has sounds, then choose a sound from there
+            -- Play an innocent/traitor team win sound if the convar is enabled
+            chosenSound = "ttt_round_end_sounds/" .. backupTeam .. "/" .. sounds[backupTeam][math.random(#sounds[backupTeam])]
+        elseif sounds["loss"][1] ~= nil and winEnum ~= WIN_NONE then
             -- If a win condition happens that's not in the "wins" table, (E.g. a new role's win), choose a random 'loss' sound to play for everyone
             chosenSound = "ttt_round_end_sounds/loss/" .. sounds["loss"][math.random(#sounds["loss"])]
         end
@@ -151,18 +85,8 @@ if engine.ActiveGamemode() == "terrortown" and SERVER then
                 net.WriteString(winSound)
                 net.WriteString(lossSound)
                 net.WriteString(chosenSound)
-                net.WriteString(oldmanWinSound)
-                net.WriteString(oldmanLossSound)
-                net.WriteInt(result, 8)
+                net.WriteInt(winEnum, 8)
                 net.Broadcast()
-            end)
-
-            -- Reset the old man win/loss sound
-            timer.Simple(5, function()
-                for i, ply in ipairs(player.GetAll()) do
-                    ply:SetNWBool("OldManWinSound", false)
-                    ply:SetNWBool("OldManLossSound", false)
-                end
             end)
         end
     end)
@@ -178,23 +102,16 @@ if engine.ActiveGamemode() == "terrortown" and CLIENT then
         end
     end
 
-    net.Receive("RoundEndSoundsPlay", function()
-        local snd = net.ReadString()
-        PlayRoundEndSound(snd)
-    end)
-
     -- Play the win sound, if any, on the client so only that player hears it
     net.Receive("RoundEndSoundsWin", function()
         local winningTeam = net.ReadString()
         local winSound = net.ReadString()
         local lossSound = net.ReadString()
         local chosenSound = net.ReadString()
-        local oldmanWinSound = net.ReadString()
-        local oldmanLossSound = net.ReadString()
-        local result = net.ReadInt(8)
+        local winEnum = net.ReadInt(8)
         local ply = LocalPlayer()
         -- Hook to let user-made roles define their own win/loss sounds, or other manipulations
-        local hookChosenSound = hook.Call("TTTChooseRoundEndSound", nil, ply, result)
+        local hookChosenSound = hook.Call("TTTChooseRoundEndSound", nil, ply, winEnum)
 
         if hookChosenSound then
             chosenSound = hookChosenSound
@@ -203,26 +120,18 @@ if engine.ActiveGamemode() == "terrortown" and CLIENT then
         -- If a special role won, play one of that role's special win sounds
         if chosenSound ~= "nosound" then
             PlayRoundEndSound(chosenSound)
-            -- Old man win/loss
-        elseif IsValid(ply) and ply:GetNWBool("OldManWinSound", false) and oldmanWinSound ~= "nosound" then
-            PlayRoundEndSound(oldmanWinSound)
-        elseif IsValid(ply) and ply:GetNWBool("OldManLossSound", false) and oldmanLossSound ~= "nosound" then
-            PlayRoundEndSound(oldmanLossSound)
         elseif winSound ~= "nosound" and lossSound ~= "nosound" and CR_VERSION then
             -- When Custom roles is installed
-            if (winningTeam == "innocent" or winningTeam == "time") and ply.IsInnocentTeam and ply:IsInnocentTeam() then
+            if (winningTeam == "innocent" or winningTeam == "timelimit") and ply.IsInnocentTeam and ply:IsInnocentTeam() then
                 PlayRoundEndSound(winSound)
             elseif winningTeam == "traitor" and ply.IsTraitorTeam and ply:IsTraitorTeam() then
-                PlayRoundEndSound(winSound)
-            elseif IsValid(ply) and ply:GetNWBool("OldManWinSound", false) then
-                -- If there's no oldmanwin sounds, play an ordinary win sound
                 PlayRoundEndSound(winSound)
             else
                 PlayRoundEndSound(lossSound)
             end
         elseif winSound ~= "nosound" and lossSound ~= "nosound" then
             -- When Custom roles isn't installed
-            if (winningTeam == "innocent" or winningTeam == "time") and (ply:GetRole() == ROLE_INNOCENT or ply:GetRole() == ROLE_DETECTIVE) then
+            if (winningTeam == "innocent" or winningTeam == "timelimit") and (ply:GetRole() == ROLE_INNOCENT or ply:GetRole() == ROLE_DETECTIVE) then
                 PlayRoundEndSound(winSound)
             elseif winningTeam == "traitor" and ply:GetRole() == ROLE_TRAITOR then
                 PlayRoundEndSound(winSound)
@@ -231,5 +140,5 @@ if engine.ActiveGamemode() == "terrortown" and CLIENT then
             end
         end
     end)
-    -- If WIN_NONE was the win result, chosenSound, winSound and lossSound are all "nosound", so nothing is played
+    -- If WIN_NONE was the winEnum, chosenSound, winSound and lossSound are all "nosound", so nothing is played
 end
